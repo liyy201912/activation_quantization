a3e9a346ddfb81ee0c17c811b710d1b2bbef5fc1
Yanyu <liyy201912@gmail.com>
2020-04-05 21:32:47 -0400

    Add files via upload

---
+++ models/vgg.py
@@ -0,0 +1,108 @@
+'''VGG11/13/16/19 in Pytorch.'''
+import torch
+import torch.nn as nn
+
+
+class BinActive(torch.autograd.Function):
+    """
+    Binarize the input activations and calculate the mean across channel dimension.
+    """
+
+    @staticmethod
+    def forward(self, input, k=4):
+        self.save_for_backward(input)
+        output = input.sign()
+        # output = torch.round(input * (2 ^ k - 1)) * (1 / (2 ^ k - 1))
+        return output
+
+    @staticmethod
+    def backward(self, grad_output):
+        input, = self.saved_tensors
+        grad_input = grad_output.clone()
+        grad_input[input.ge(1)] = 0
+        grad_input[input.le(-1)] = 0
+        return grad_input
+
+
+class BinConv2d(nn.Module):  # change the name of BinConv2d
+    def __init__(self, input_channels, output_channels,
+                 kernel_size=-1, stride=-1, padding=-1, groups=1, dropout=0,
+                 Linear=False):
+        super(BinConv2d, self).__init__()
+        self.layer_type = 'BinConv2d'
+        self.kernel_size = kernel_size
+        self.stride = stride
+        self.padding = padding
+        self.dropout_ratio = dropout
+
+        if dropout != 0:
+            self.dropout = nn.Dropout(dropout)
+        self.Linear = Linear
+        if not self.Linear:
+            self.bn = nn.BatchNorm2d(input_channels, eps=1e-4, momentum=0.1, affine=True)
+            self.conv = nn.Conv2d(input_channels, output_channels,
+                                  kernel_size=kernel_size, stride=stride, padding=padding, groups=groups)
+        else:
+            self.bn = nn.BatchNorm1d(input_channels, eps=1e-4, momentum=0.1, affine=True)
+            self.linear = nn.Linear(input_channels, output_channels)
+        self.relu = nn.ReLU(inplace=True)
+
+    def forward(self, x):
+        x = self.bn(x)
+        x = BinActive.apply(x)
+        if self.dropout_ratio != 0:
+            x = self.dropout(x)
+        if not self.Linear:
+            x = self.conv(x)
+        else:
+            x = self.linear(x)
+        x = self.relu(x)
+        return x
+
+
+cfg = {
+    'VGG11': [64, 'M', 128, 'M', 256, 256, 'M', 512, 512, 'M', 512, 512, 'M'],
+    'VGG13': [64, 64, 'M', 128, 128, 'M', 256, 256, 'M', 512, 512, 'M', 512, 512, 'M'],
+    'VGG16': [64, 64, 'M', 128, 128, 'M', 256, 256, 256, 'M', 512, 512, 512, 'M', 512, 512, 512, 'M'],
+    'VGG19': [64, 64, 'M', 128, 128, 'M', 256, 256, 256, 256, 'M', 512, 512, 512, 512, 'M', 512, 512, 512, 512, 'M'],
+}
+
+
+class VGG(nn.Module):
+    def __init__(self, vgg_name):
+        super(VGG, self).__init__()
+        self.features = self._make_layers(cfg[vgg_name])
+        self.classifier = BinConv2d(512, 10, Linear=True)
+
+    def forward(self, x):
+        out = self.features(x)
+        out = out.view(out.size(0), -1)
+        out = self.classifier(out)
+        return out
+
+    def _make_layers(self, cfg):
+        layers = []
+        in_channels = 3
+        for x in cfg:
+            if x == 'M':
+                layers += [nn.MaxPool2d(kernel_size=2, stride=2)]
+            else:
+                layers += [BinConv2d(in_channels, x, kernel_size=3, stride=1, padding=1)]
+                in_channels = x
+        layers += [nn.AvgPool2d(kernel_size=1, stride=1)]
+        return nn.Sequential(*layers)
+
+
+def test():
+    net = VGG('VGG16')
+    x = torch.randn(2, 3, 32, 32)
+    y = net(x)
+    print(y.size())
+    name_list = []
+    for name, weights in net.named_parameters():
+        if 'conv.weight' in name:
+            name_list.append(name)
+    print(name_list)
+
+
+# test()
