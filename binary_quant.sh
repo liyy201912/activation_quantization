7226cff02b66dec1dce18b9640345377c6e66fba
Yanyu <liyy201912@gmail.com>
2020-06-02 17:43:54 -0400

    Delete binary_quant.sh

--- binary_quant.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-export CUDA_VISIBLE_DEVICES=0
-
-python3.6 -u main.py --epoch 150 --admm-quant --quant-type random_binary --resume "./checkpoints/cifar10_vgg16_bn_acc_93.92.pt" -a vgg16_bn --verbose
